ebed07c25ff7a2fcaa897b2d7810fa065c14e2e6
Mishin870 <polsit@yandex.ru>
2017-10-16 20:55:10 +0400

    merge

--- bot/config.py
+++ bot/config.py
@@ -1,2 +1,8 @@
+<<<<<<< HEAD
 token = '423620134:AAEDn7fPemg9lqMlWlFtq5UNXpf99UyyTvc' #тестовый бот
 # token = '456170128:AAFNMPAfJ3B84t4vDdaSilfJZYgm-q3i7mk' #каргобот
+=======
+#token = '423620134:AAEDn7fPemg9lqMlWlFtq5UNXpf99UyyTvc'
+token = '456170128:AAFNMPAfJ3B84t4vDdaSilfJZYgm-q3i7mk'
+site_base = 'http://5.63.154.237/'
+>>>>>>> fonts
