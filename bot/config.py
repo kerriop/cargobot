d12cd7a4898dec99329e06100933b23c0ad2fc9b
kerriop <nor808@yandex.ru>
2017-10-16 18:40:15 +0300

    токены токены 2

--- bot/config.py
+++ bot/config.py
@@ -1,2 +1,2 @@
-#token = '423620134:AAEDn7fPemg9lqMlWlFtq5UNXpf99UyyTvc' #тестовый бот
-token = '456170128:AAFNMPAfJ3B84t4vDdaSilfJZYgm-q3i7mk' #каргобот
+token = '423620134:AAEDn7fPemg9lqMlWlFtq5UNXpf99UyyTvc' #тестовый бот
+# token = '456170128:AAFNMPAfJ3B84t4vDdaSilfJZYgm-q3i7mk' #каргобот
